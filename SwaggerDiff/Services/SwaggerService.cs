ac266c178e9429fd3232894fa27eb3b8ae0dc2c5
Malcolm Navarro <mnavarro@templuminc.com>
2019-10-18 18:53:55 -0400

    Got the swagger task to work with an http call

--- SwaggerDiff/Services/SwaggerService.cs
+++ SwaggerDiff/Services/SwaggerService.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using Microsoft.Extensions.Logging;
 
 using SwaggerDiff.Models;
@@ -8,23 +9,35 @@ namespace SwaggerDiff.Services
 {
     public class SwaggerService : ISwaggerService
     {
-        private readonly SwaggerDiffContext _context;
         private readonly ILogger _logger;
+        private readonly SwaggerDiffContext _context;
+        private readonly IHttpClientFactory _clientFactory;
 
-        public SwaggerService(ILogger<SwaggerService> logger, SwaggerDiffContext context)
+
+        public SwaggerService(ILogger<SwaggerService> logger, SwaggerDiffContext context, IHttpClientFactory clientFactory)
         {
             _logger = logger;
             _context = context;
+            _clientFactory = clientFactory;
         }
 
-        public void Initialize()
+        public async void Initialize()
         {
+
             _logger.LogInformation("Hello from Swagger Service");
 
             List<SwaggerItem> swaggerItems = _context.SwaggerItems.ToList();
 
             _logger.LogInformation(swaggerItems.ToString());
-        
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.GetAsync("https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v2.0/json/petstore-simple.json");
+
+            response.EnsureSuccessStatusCode();
+
+            _logger.LogInformation($"JSON EXAMPLE:\n {response}");
+
             // TODO: Execute the HTTP requests and add them to the in-memory DB
         }
     }
--- SwaggerDiff/Startup.cs
+++ SwaggerDiff/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 using SwaggerDiff.Models;
 using SwaggerDiff.Services;
@@ -28,29 +29,37 @@ namespace SwaggerDiff
       // Add SwaggerDiff controllers
       services.AddControllers();
 
-      // Register swagger service to run
-      services.AddSingleton<ISwaggerService, SwaggerService>(); 
+      // Register swagger service
+      services.AddSingleton<ISwaggerService, SwaggerService>();
       // TODO: Learn difference between AddSingleton, AddTransient, (one other)
+
+      // Registering the swagger services with http client (DI)
+      services.AddHttpClient();
+
+      // services.AddHttpClient(<ISwaggerService, SwaggerService); TODO: Why doesn't this work but the above does?
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
+      // Captures exception instances from the pipeline and generates HTML error responses
+      if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
+      // Middleware for redirecting HTTP Requests to HTTPS
       app.UseHttpsRedirection();
 
       app.UseRouting();
 
+      // Enable authorization capabilities
       app.UseAuthorization();
 
       app.UseEndpoints(endpoints => endpoints.MapControllers());
 
-      // Blocking task to get all the swagger JSONs
-      app.ApplicationServices.GetService<ISwaggerService>().Initialize();
+      // Fire off async task to get all the swagger JSONs
+      Task initializeSwaggerTask = Task.Run(() => app.ApplicationServices.GetService<ISwaggerService>().Initialize());
+
+      // Wait until initialize swagger task is complete
+      initializeSwaggerTask.Wait();
     }
   }
 }
