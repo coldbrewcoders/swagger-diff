e34604f63c44b640e7b9b970616c88d37a36d152
Andrew Hnilica <ahnilica@templuminc.com>
2019-11-13 02:42:11 -0500

    Started the swagger diff algorithm for detecting the addition of routes and http methods for those routes. Realized that in memory DB is not thread safe and is only recommended for testing, explored options for block a service-specific webhook until completed

--- Services/ICompareService.cs
+++ Services/ICompareService.cs
@@ -1,8 +1,11 @@
+using System.Threading.Tasks;
+
+
 namespace SwaggerDiff.Services
 {
     public interface ICompareService
     {
         bool AreJSONDocumentsIdentical(string previousJSON, string freshJSON);
-        void CheckServiceForApiChanges(string previousJSON, string freshJSON);
+        Task CheckServiceForApiChanges(string previousJSON, string freshJSON);
     }
 }
